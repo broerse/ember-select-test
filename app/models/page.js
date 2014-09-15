c2af63530bf9eca687ad10eb4d5a9df2ac2da051
Martin Broerse <broerse@martinic.com>
2014-09-15 16:23:55 +0200

    Add modelname

--- app/models/page.js
+++ app/models/page.js
@@ -4,7 +4,8 @@ var Page = DS.Model.extend({
 	title: DS.attr('string', {defaultValue: ""}),
 	date: DS.attr('date'),
 	body: DS.attr('string', {defaultValue: ""}),
-	rev: DS.attr('string')
+	rev: DS.attr('string'),
+	modelname: DS.attr('string', {defaultValue: ""})
 });
 
 export default Page;
