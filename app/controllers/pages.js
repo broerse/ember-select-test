92f50fd7aa699ebffbbf17d2dbdc669d76fd8f16
Martin Broerse <broerse@martinic.com>
2014-09-28 18:32:48 +0200

    Fix ,

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,11 +1,12 @@
 import Ember from "ember";
 
-export default Ember.ObjectController.extend({
+export default Ember.ArrayController.extend({
   actions: {
     createPage: function() {
       var newPage = this.get('store').createRecord('page');
       newPage.set('date' , new Date());
       this.get('target').transitionTo('page', newPage.save());
     }
-  }
+  },
+  sortProperties: ['title']
 });
