f3a4ff1887f9f9564b5e391c37159b8f2ca7cec7
Martin Broerse <broerse@martinic.com>
2015-06-11 13:00:48 +0200

    ember-data canary not working yet

--- app/models/modelname.js
+++ app/models/modelname.js
@@ -3,7 +3,7 @@ import { Model } from 'ember-pouch';
 
 var Modelname = Model.extend({
 	name: DS.attr('string', {defaultValue: ""}),
-	pages: DS.hasMany('page', {serialize: true})
+	pages: DS.hasMany('page', {async: true, serialize: true})
 });
 
 export default Modelname;
--- app/routes/pages.js
+++ app/routes/pages.js
@@ -1,16 +1,6 @@
 import Ember from "ember";
 export default Ember.Route.extend({
 	model: function() {
-		return this.store.find('page');
-	},
-	afterModel: function (recordArray) {
-    // This tells PouchDB to listen for live changes and
-    // notify Ember Data when a change comes in.
-    new PouchDB('selecttest').changes({
-      since: 'now',
-      live: true
-    }).on('change', function () {
-      recordArray.update();
-    });
-  }
+		return this.store.findAll('page');
+	}
 });
